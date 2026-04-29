515f0471227531b820fb1f2b37b441da048a29c9
kendraid001 <kendraid001@gmail.com>
2026-04-29 11:28:00 -0400

    day 1

--- tile.py
+++ tile.py
@@ -9,13 +9,20 @@ class Tile(pygame.sprite.Sprite):
         super().__init__()
 
         # TODO: assign pygame.transform.scale() to self.image.  The scale() function call gets the following are arguments
+        self.image = pygame.transform.scale(pygame.image.load(f"images/tiles/Tile ({image_int}).png"),(32, 32))
         # pygame.image.load(f"images/tiles/Tile ({image_int}).png")
         # (32, 32)
 
         # TODO: if sub_group is not None:
+        if sub_group is not None:
             # TODO: call sub_group.add() passing in self as the argument
+            sub_group.add(self)
         # TODO: call main_group.add() passing in self as the argument.
+        main_group.add(self)
 
         # TODO: assign self.image.get_rect() to self.rect
+        self.rect = self.image.get_rect()
         # TODO: assign (x, y) to self.rect.topleft
+        self.rect.topleft = (x, y)
         # TODO: assign pygame.mask.from_surface() to self.mask  The from_surface() function call gets self.image as its argument
+        self.mask = pygame.mask.from_surface(self.image)
